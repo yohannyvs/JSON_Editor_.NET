f00be5ce3bc2bdb3161dc03e686d496ad14686f0
yohannyvs <yohannyvs@gmail.com>
2017-07-10 18:11:16 -0600

    act 2
    Clase Oferente y metodos Get

---
+++ JSON_Editor/JSON_Editor/Oferentes.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Editor
+{
+    class Oferentes
+    {
+
+        private String nombre;
+        private String apellido;
+        private String direccion;
+        private String correo;
+        private String profesion;
+        private String puesto_solicitante;
+        private int salario_base;
+        private String ultimo_trabajo;
+
+        public Oferentes() { }
+
+        public Oferentes(string nombre, string apellido, string direccion, string correo, string profesion, string puesto_solicitante, int salario_base, string ultimo_trabajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.direccion = direccion;
+            this.correo = correo;
+            this.profesion = profesion;
+            this.puesto_solicitante = puesto_solicitante;
+            this.salario_base = salario_base;
+            this.ultimo_trabajo = ultimo_trabajo;
+        }
+
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        public string getApellido()
+        {
+            return this.apellido;
+        }
+
+        public string getDreccion()
+        {
+            return this.direccion;
+        }
+
+        public string getCorreo()
+        {
+            return this.correo;
+        }
+
+        public string getProfecion()
+        {
+            return this.profesion;
+        }
+
+        public string getPuesto_Solicitante()
+        {
+            return this.puesto_solicitante;
+        }
+
+        public int getSalario_Base()
+        {
+            return this.salario_base;
+        }
+
+        public string getUltimo_Trabajo()
+        {
+            return this.ultimo_trabajo;
+        }
+    }
+}
