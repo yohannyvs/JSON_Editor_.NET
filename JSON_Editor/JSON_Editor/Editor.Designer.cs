662307891fc32e31c8d245eea9e50531baa05e07
yohannyvs <yohannyvs@gmail.com>
2017-07-10 19:32:26 -0600

    act 4
    Diseño basico de IDE

--- JSON_Editor/JSON_Editor/Editor.Designer.cs
+++ JSON_Editor/JSON_Editor/Editor.Designer.cs
@@ -28,11 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_apellido = new System.Windows.Forms.TextBox();
+            this.txt_direccion = new System.Windows.Forms.TextBox();
+            this.txt_correo = new System.Windows.Forms.TextBox();
+            this.txt_prof = new System.Windows.Forms.TextBox();
+            this.txt_puest_sol = new System.Windows.Forms.TextBox();
+            this.txt_sal_base = new System.Windows.Forms.TextBox();
+            this.txt_ult_trab = new System.Windows.Forms.TextBox();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellido:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Dirección:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Correo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Profesión:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Puesto Solicitado:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(22, 250);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Salario Base:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(22, 283);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Ultimo Trabajo";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(130, 16);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(152, 20);
+            this.txt_id.TabIndex = 9;
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(130, 44);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(152, 20);
+            this.txt_nombre.TabIndex = 10;
+            // 
+            // txt_apellido
+            // 
+            this.txt_apellido.Location = new System.Drawing.Point(130, 76);
+            this.txt_apellido.Name = "txt_apellido";
+            this.txt_apellido.Size = new System.Drawing.Size(152, 20);
+            this.txt_apellido.TabIndex = 11;
+            // 
+            // txt_direccion
+            // 
+            this.txt_direccion.Location = new System.Drawing.Point(130, 109);
+            this.txt_direccion.Name = "txt_direccion";
+            this.txt_direccion.Size = new System.Drawing.Size(152, 20);
+            this.txt_direccion.TabIndex = 12;
+            // 
+            // txt_correo
+            // 
+            this.txt_correo.Location = new System.Drawing.Point(130, 142);
+            this.txt_correo.Name = "txt_correo";
+            this.txt_correo.Size = new System.Drawing.Size(152, 20);
+            this.txt_correo.TabIndex = 13;
+            // 
+            // txt_prof
+            // 
+            this.txt_prof.Location = new System.Drawing.Point(130, 174);
+            this.txt_prof.Name = "txt_prof";
+            this.txt_prof.Size = new System.Drawing.Size(152, 20);
+            this.txt_prof.TabIndex = 14;
+            // 
+            // txt_puest_sol
+            // 
+            this.txt_puest_sol.Location = new System.Drawing.Point(130, 207);
+            this.txt_puest_sol.Name = "txt_puest_sol";
+            this.txt_puest_sol.Size = new System.Drawing.Size(152, 20);
+            this.txt_puest_sol.TabIndex = 15;
+            // 
+            // txt_sal_base
+            // 
+            this.txt_sal_base.Location = new System.Drawing.Point(130, 243);
+            this.txt_sal_base.Name = "txt_sal_base";
+            this.txt_sal_base.Size = new System.Drawing.Size(152, 20);
+            this.txt_sal_base.TabIndex = 16;
+            // 
+            // txt_ult_trab
+            // 
+            this.txt_ult_trab.Location = new System.Drawing.Point(130, 280);
+            this.txt_ult_trab.Name = "txt_ult_trab";
+            this.txt_ult_trab.Size = new System.Drawing.Size(152, 20);
+            this.txt_ult_trab.TabIndex = 17;
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.Location = new System.Drawing.Point(318, 23);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(93, 32);
+            this.btn_agregar.TabIndex = 18;
+            this.btn_agregar.Text = "Agregar";
+            this.btn_agregar.UseVisualStyleBackColor = true;
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.Location = new System.Drawing.Point(318, 76);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(93, 32);
+            this.btn_guardar.TabIndex = 19;
+            this.btn_guardar.Text = "Guardar";
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            // 
+            // Editor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 323);
+            this.Controls.Add(this.btn_guardar);
+            this.Controls.Add(this.btn_agregar);
+            this.Controls.Add(this.txt_ult_trab);
+            this.Controls.Add(this.txt_sal_base);
+            this.Controls.Add(this.txt_puest_sol);
+            this.Controls.Add(this.txt_prof);
+            this.Controls.Add(this.txt_correo);
+            this.Controls.Add(this.txt_direccion);
+            this.Controls.Add(this.txt_apellido);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Editor";
             this.Text = "Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_apellido;
+        private System.Windows.Forms.TextBox txt_direccion;
+        private System.Windows.Forms.TextBox txt_correo;
+        private System.Windows.Forms.TextBox txt_prof;
+        private System.Windows.Forms.TextBox txt_puest_sol;
+        private System.Windows.Forms.TextBox txt_sal_base;
+        private System.Windows.Forms.TextBox txt_ult_trab;
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.Button btn_guardar;
     }
 }
